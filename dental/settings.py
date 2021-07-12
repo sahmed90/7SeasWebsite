ad38d9f3dfd03cf2ecfbf599efe5cffc9b74e366
Saad Ahmed <sahmed2190@gmail.com>
2021-07-12 14:51:27 +0400

    tweaked app for heroku

--- dental/settings.py
+++ dental/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'dental.urls'
@@ -125,6 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -144,3 +150,6 @@ EMAIL_USE_SSL = False
 # EMAIL_HOST_PASSWORD = ''
 # # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
+
+
+django_heroku.settings(locals())
